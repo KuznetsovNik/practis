1faf89f81566bf3f9ff68d77da2b39a819a1e1d8
HomeAdmin <xax_xax@list.ru>
2020-05-27 16:13:39 +0300

    Rework2 IntegerExperiments

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Container.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Container.java
@@ -1,35 +1,32 @@
 public class Container
 {
+     /** Схема
+      * В метод поступает (число)
+      * Число преобразовать в строку
+      * Разложить это строку на индексы (инд0=ч ; инд1=и ; инд2=с ; инд3=л ; инд4=о)
+      * Каждый индекс преобразовать в строку, а затем строку в число
+      * Сложить эти индексы-строки арифметически
+      * Возратить сумму
+      *
+      * Методы:
+      * .toString() - преобразовывает число в строку
+      * .length() - считает длину строки
+      * .charAt(i) - разбивает строку на символы по индексам
+      * .parseInt - преобразовывает строку в число
+      * .valueOf - преобразовывает любой объект в строку
+     **/
     public int count;
 
-    /** Добиться чтобы это метод считал сумму вводных данных
-     *  Например если внутри параметра метода поставить 575
-     * Он будет печатать в консоле 17
-     * */
     public Integer sumDigits(Integer number) {
-        int amount;
+        int amount = 0;
         String integerString = number.toString();
-        Character character0 = integerString.charAt(0);
-        Character character1 = integerString.charAt(1);
-        Character character2 = integerString.charAt(2);
-        Character character3 = integerString.charAt(3);
-        Character character4 = integerString.charAt(4);
-        for (;0 < integerString.length();){
-            Integer index0 = Integer.parseInt(String.valueOf(character0));
-            Integer index1 = Integer.parseInt(String.valueOf(character1));
-            Integer index2 = Integer.parseInt(String.valueOf(character2));
-            Integer index3 = Integer.parseInt(String.valueOf(character3));
-            Integer index4 = Integer.parseInt(String.valueOf(character4));
-            amount = index0 + index1 + index2 + index3 + index4;
+        for (int i = 0; i < integerString.length(); i++){
+            char charAt = integerString.charAt(i);
+            System.out.printf("Символ номер %d в строке '%s' = %c%n", i, integerString, charAt);
+            Integer index = Integer.parseInt(String.valueOf(charAt));
+            amount += index;
         }
-        return amount;
-        /** Схема
-         * В метод поступает (число)
-         * Число преобразовать в строку # 11
-         * Разложить это строку на индексы (инд0=ч ; инд1=и ; инд2=с ; инд3=л ; инд4=о) # 12 - 16
-         * Каждый индекс преобразовать в строку, а затем строку в число  # 17 - 21
-         * Сложить эти индексы-строки арифметически # 22
-         * Возратить сумму # 23
-         */
+        System.out.print("Cумма всех чисел = ");
+         return amount;
     }
 }
