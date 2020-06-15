2f3b4f1cd048477023f1bbf313c99ebe239e9965
HomeAdmin <xax_xax@list.ru>
2020-06-15 23:04:30 +0300

    Rework2 birthday

--- 04_NumbersStringsAndDates/birthday/src/Birthday.java
+++ 04_NumbersStringsAndDates/birthday/src/Birthday.java
@@ -4,15 +4,16 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 public class Birthday {
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy - EEEE");
     public static void main(String[] args) {
         LocalDate now = LocalDate.of(1995, Month.OCTOBER, 27);
         int birthday = 0;
-        for (int i = 0; i < 100; i++) {
-            System.out.printf("%d %s \n", birthday, now.plus(Period.ofYears(i)).format(DateTimeFormatter.ofPattern("dd.MM.yyyy - EEEE")));
+        while (now.isBefore(LocalDate.now())){
+            System.out.printf("%d %s \n", birthday, now.plus(Period.ofYears(1)).format(DATE_FORMAT));
             birthday++;
-            if (now.plus(Period.ofYears(i+1)).isAfter(LocalDate.now())){
-                break;
-            }
+            //if (now.plus(Period.ofYears(i+1)).isAfter(LocalDate.now())) {
+              //  break;
+            //}
         }
     }
 }
