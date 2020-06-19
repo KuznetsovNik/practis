2fdb8fef838247d84169925044624be84fdac457
HomeAdmin <xax_xax@list.ru>
2020-06-19 21:14:35 +0300

    Rework3 regExp

--- 04_NumbersStringsAndDates/telephoneNumbers/src/telephoneNumbers.java
+++ 04_NumbersStringsAndDates/telephoneNumbers/src/telephoneNumbers.java
@@ -7,11 +7,11 @@ public class telephoneNumbers {
         System.out.println("Введи номер телефона");
         Scanner scanner = new Scanner(System.in);
         String number = scanner.nextLine();
-        String pattern = number.replaceAll("[\\Da-zA-Z\\s]", "");
+        String pattern = number.replaceAll("[^\\d]","" );
         if (pattern.length() == 11) {
-            System.out.println(pattern.replaceAll("^[8]","7"));
+            System.out.println(pattern.replaceAll("^8(.{10})$", "7$1"));
         }else if (pattern.length() == 10){
-            System.out.println("7" + pattern);
+           System.out.println("7" + pattern);
         } else {
             System.out.println("Неверный формат номера");
         }
