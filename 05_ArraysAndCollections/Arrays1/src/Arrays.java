208767cffb7a723f04e89a0187219bf2c58af850
HomeAdmin <xax_xax@list.ru>
2020-11-10 15:59:16 +0300

    ReworkArrays4

--- 05_ArraysAndCollections/Arrays1/src/Arrays.java
+++ 05_ArraysAndCollections/Arrays1/src/Arrays.java
@@ -1,22 +1,21 @@
 public class Arrays {
     public static void main(String[] args) {
-        String text = "Каждый охотник желает знать, где сидит фазан";
-        System.out.println(text);
-        String [] arrays = text.split(",?\\s+");
-        int [] index = new int[7];
-        index [0] = 6;
-        index [1] = 5;
-        index [2] = 4;
-        index [3] = 3;
-        index [4] = 2;
-        index [5] = 1;
-        index [6] = 0;
-        int x = 0;
-        int replace;
-        while (x < 7){
-            replace = index[x];
-            System.out.print(" " + arrays[replace]);
-            x = x + 1;
+        String text = "Каждый охотник желает знать, где сидит фазан";  // Строка text с изначальной фразой
+        System.out.println(text);   // Печать text
+        String[] arrays = text.split(",?\\s+"); // Создание массива строк, разбитием регулярным выражением
+
+        int index = 1; // Переменная для отступания с конца строки
+        String tmp; // Временная строка для замены местами начала и конца
+
+        for (int i = 0; i < arrays.length/2 ; i++) { // Цикл для замены мест, делим на 2 длинну для опеределения середины
+            tmp = arrays[i];  //
+            arrays[i] = arrays[arrays.length - index];
+            arrays[arrays.length - index] = tmp;
+            index++;
+        }
+
+        for (int i = 0; i < arrays.length; i++){  // Печать
+            System.out.print(arrays[i] + " ");
         }
     }
 }
--- 05_ArraysAndCollections/ProjectX/src/ProjectX.java
+++ 05_ArraysAndCollections/ProjectX/src/ProjectX.java
@@ -1,25 +1,27 @@
+
 public class ProjectX {
 
-    public static final int SIZE_ARRAYS = 3;
+    public static final int SIZE_ARRAYS = 7;  // Константа на размер креста
 
     public static void main(String[] args) {
-        int finalSize = SIZE_ARRAYS;
-        String [][] projectX =
-                {{"X"," "," "," "," "," ","X"},
-                {" ","X"," "," "," ","X"," "},
-                {" "," ","X"," ","X"," "," "},
-                {" "," "," ","X"," "," "," "},
-                {" "," ","X"," ","X"," "," "},
-                {" ","X"," "," "," ","X"," "},
-                {"X"," "," "," "," "," ","X"}};
-        while (finalSize > 0) {
-            for (int i = 0; i < projectX.length; i++) {
-                System.out.println(" ");
-                for (int j = 0; j < projectX[i].length; j++) {
-                    System.out.print("\t" + projectX[i][j]);
+        int finalSize = SIZE_ARRAYS; // Инкапсуляция константы
+        String [][] projectX = new String[finalSize][finalSize]; // Создаём массив массивов строк с именем projectX
+        int index = 1; // Переменная для создания второй диагонали перекрестия
+        for (int i = 0; i < projectX.length ; i++) { // Перебераем колличество массивов
+            for (int j = 0; j < projectX.length; j++) { // Перебераем и присваиваем строки в массивах
+                if (i == j || j == projectX.length - index) {
+                    projectX[i][j] = "x";
+                } else {
+                    projectX[i][j] = " ";
                 }
             }
-            finalSize--;
+            index++;
+        }
+        for (int x = 0; x < projectX.length; x++) { // Цикл для печати креста
+            System.out.println();
+            for (int y = 0; y < projectX[x].length; y++) {
+                System.out.print("\t" + projectX[x][y]);
+            }
         }
     }
 }
