af2ae212cb6cab53586812e36dcdb6f90105e57b
HomeAdmin <xax_xax@list.ru>
2020-11-12 23:59:14 +0300

    ReworkArrayList2

--- 05_ArraysAndCollections/ArrayList1/src/ArrayList1.java
+++ 05_ArraysAndCollections/ArrayList1/src/ArrayList1.java
@@ -4,75 +4,52 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ArrayList1 {
-        public static void main (String[]args){
-        System.out.println("Здравствуйте, это список дел. Введите одну из команд:");
-        System.out.println("LIST - список дел\nADD - добавить заметку\nEDIT - заменить\nDELETE - удалить");
+        public static void main (String[]args) {
+            System.out.println("Здравствуйте, это список дел. Введите одну из команд:");
+            System.out.println("LIST - список дел\nADD - добавить заметку\nEDIT - заменить\nDELETE - удалить");
             Scanner scanner = new Scanner(System.in);
             String toDo = scanner.nextLine();
-            ArrayList<String> toDoList = new ArrayList<>();
+            ArrayList<String> toDoList = new ArrayList<>(); // Создаём список элементов
 
-            // Распечатка списка дел
-            Pattern patternList = Pattern.compile("(?i)list");
-            Matcher matcherList = patternList.matcher(toDo);
-            if (matcherList.find()) {
+            // Команда печати списка дел
+            Matcher matcherList = Pattern.compile("^(?i)list").matcher(toDo);
+            if (matcherList.matches()) {
                 for (int i = 0; i < toDoList.size(); i++) {
                     System.out.printf("%d %s\n", i, toDoList.get(i));
                 }
             }
 
-            // Добавление заметки
-            Pattern patternAdd = Pattern.compile("(?<Add>(?i)add)|\\d+");
-            Matcher matcherAdd = patternAdd.matcher(toDo);
-            if (matcherAdd.find()) {
-                    if (toDo.replaceAll("[^\\d]", "").trim().isEmpty()) {
-                        toDoList.add(toDo.replaceAll(matcherAdd.group("Add"), ""));
-                    } else {
-                        int number = Integer.parseInt(toDo.replaceAll("[^\\d]", ""));
-                        if (number != toDoList.size()) {
-                            number = toDoList.size();
-                            toDoList.add(number, toDo.replaceAll("(?i)add|(\\d)*" , ""));
-                        }
-                    }
+            // Команнда добавления заметки
+            Matcher matcherAddIndexed = Pattern.compile("^(?i)add (?<NoDeal>\\d+) (?<text>.+)").matcher(toDo);
+            Matcher matcherAdd = Pattern.compile("^(?i)add (?<text>.+)").matcher(toDo);
+            if (matcherAddIndexed.matches()) {
+                int number = Integer.parseInt(matcherAddIndexed.group("NoDeal"));
+                if (number != toDoList.size()) {
+                    number = toDoList.size();
+                    toDoList.add(number, matcherAddIndexed.group("text"));
+                }
+            } else if (matcherAdd.matches()) {
+                toDoList.add(matcherAdd.group("text"));
             }
 
-            // Замена заметки
-            Pattern patternEdit = Pattern.compile("(?i)edit");
-            Matcher matcherEdit = patternEdit.matcher(toDo);
-            if (matcherEdit.find()) {
+            // Команда на замену заметки
+            Matcher matcherEdit = Pattern.compile("^(?i)edit").matcher(toDo);
+            if (matcherEdit.matches()) {
                 int where = Integer.parseInt(toDo.replaceAll("[^\\d]", ""));
                 String text = toDo.replaceAll("(?i)edit|\\d", "");
                 toDoList.add(where, text);
                 toDoList.remove(where + 1);
             }
 
-            // Удаление
-            Pattern patternDelete = Pattern.compile("(?i)delete");
-            Matcher matcherDelete = patternDelete.matcher(toDo);
-            if (matcherDelete.find()) {
+            // Команда удаления заметки
+            Matcher matcherDelete = Pattern.compile("^(?i)delete").matcher(toDo);
+            if (matcherDelete.matches()) {
                 int index = Integer.parseInt(toDo.replaceAll("[^\\d]", ""));
                 if (index > toDoList.size()) {
                     toDoList.remove(toDoList.size() - 1);
-                }else {
+                } else {
                     toDoList.remove(index);
                 }
             }
-            
-        /**
-         * Разработайте список дел, который управляется командами в консоли. Команды: LIST, ADD, EDIT, DELETE.
-         *
-         * Принцип работы команд:
-         *
-         * LIST — выводит дела с их порядковыми номерами;
-         * ADD — добавляет дело в конец списка или дело на определённое место, сдвигая остальные дела вперёд, если указать номер;
-         * EDIT — заменяет дело с указанным номером;
-         * DELETE — удаляет.
-         * Примеры команд:
-         *
-         * LIST
-         * ADD Какое-то дело
-         * ADD 4 Какое-то дело на четвёртом месте
-         * EDIT 3 Новое название дела
-         * DELETE 7
-         **/
-    }
+        }
 }
