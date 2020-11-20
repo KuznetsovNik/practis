9dc8b0c25bdf2c2e44bd62012d33503b9cb41390
HomeAdmin <xax_xax@list.ru>
2020-11-21 00:36:46 +0300

    ReworkMaPhoneBook1

--- 05_ArraysAndCollections/MapTelefonBook/src/MapBook.java
+++ 05_ArraysAndCollections/MapTelefonBook/src/MapBook.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
@@ -5,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class MapBook {
     public static void main(String[] args) {
-        TreeMap<String , Integer> name2number = new TreeMap<>();
+        TreeMap<String , Long > name2number = new TreeMap<>();
         Scanner scanner = new Scanner(System.in);
 
         for (;;) {
@@ -14,8 +15,8 @@ public class MapBook {
             // Команда на печать контаков
             Matcher matcherList = Pattern.compile("^(?i)list").matcher(contact);
             if (matcherList.matches()) {
-                for (String key : name2number.keySet()) {
-                    System.out.println(key + " - " + name2number.get(key));
+                for (Map.Entry<String , Long> entry : name2number.entrySet()){
+                    System.out.println(entry.getKey() + " - " + entry.getValue());
                 }
                 continue;
             }
@@ -23,28 +24,30 @@ public class MapBook {
             // Команда добавления контакта
             Matcher matcherAddAll = Pattern.compile("(?<number>\\d+) (?<person>.+)").matcher(contact);
             Matcher matcherAddNumber = Pattern.compile("(?<number>\\d+)").matcher(contact);
+            Matcher matcherAddPerson = Pattern.compile("(?<person>.+)").matcher(contact);
             if (matcherAddAll.matches()) {
-                name2number.put(matcherAddAll.group("person"), Integer.parseInt(matcherAddAll.group("number")));
+                name2number.put(matcherAddAll.group("person") , Long.parseLong(matcherAddAll.group("number")) );
             } else if (matcherAddNumber.matches()) {
                 // Проверка на существование номера
-                if (name2number.containsValue(Integer.parseInt(contact))) {
-                    System.out.println(contact + " - " + name2number.keySet());
+                if (name2number.containsValue(Long.parseLong(contact))) {
+                    System.out.println(name2number.get(contact) + " - " + contact);
                     continue;
                 }
                 // Просьба дополнить контакт
                 System.out.println("Напишите Имя абонента");
                 String name = scanner.nextLine();
-                name2number.put(name,Integer.parseInt(matcherAddNumber.group("number")));
-            } else {
+                name2number.put(name , Long.parseLong(matcherAddNumber.group("number")));
+            } else if (matcherAddPerson.matches()){
                 // Проверка на существование имени
                 if (name2number.containsKey(contact)) {
-                    System.out.println(contact + " - " + name2number.values());
+                    System.out.println(name2number.get(contact) + " - " + contact);
                     continue;
                 }
                 // Просьба дополнить контакт
                 System.out.println("Напишите номер абонента");
                 String phone = scanner.nextLine();
-                name2number.put(matcherAddAll.group("person"),Integer.parseInt(phone));
+                name2number.put(matcherAddPerson.group("person") , Long.parseLong(phone));
+
             }
         }
     }
