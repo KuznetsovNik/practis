2443ceb94b2cb885e259d8de1cd1adfdba196b88
HomeAdmin <xax_xax@list.ru>
2021-03-27 21:59:36 +0300

    ReworkFilesAndNetwork.1

--- 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -3,22 +3,26 @@ import java.io.File;
 public class FileUtils {
 
     public static long calculateFolderSize(String path){
-        long weight = 0;
+        long weight = 0L;
         File file = new File(path);
         try{
             if (file.isDirectory()){
                 System.out.println("Размер папки - " + file.getName() + " составляет - " + friendlyFileSize(file.length()));
+                File[] files = file.listFiles();
+                for(int i = 0; i < file.length(); i++){
+                    weight += calculateFolderSize(files[i].getPath());
+                }
             }else {
                 System.out.println("Размер файла - " + file.getName() + " составляет - " + friendlyFileSize(file.length()));
+                weight += file.length();
             }
-            weight += file.length();
         }catch (Exception ex){
             ex.printStackTrace();
         }
         return weight;
     }
 
-    private static String friendlyFileSize(long size){
+    public static String friendlyFileSize(long size){
         String unit = "байт";
         if (size > 1024){
             size = size/1024;
--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -6,32 +5,9 @@ public class Main {
         System.out.println("Введите путь к папке");
         Scanner scanner = new Scanner(System.in);
         String path = scanner.nextLine();
-        File directory = new File(path);
-        try {
-            FileUtils.calculateFolderSize(directory.getPath());
-            System.out.println("В ней содержатся:");
-            if (directory.isDirectory()) {
-                for (File folder : directory.listFiles()) {
-                    if (folder.isDirectory()) {
-                        System.out.println("\n \t Папка: " + folder.getName());
-                        FileUtils.calculateFolderSize(folder.getPath());
-                        System.out.println("В ней содержатся:");
-                        for (File file : folder.listFiles()) {
-                            if (file.isDirectory()) {
-                                System.out.println("\t Папка:" + file.getName());
-                            } else {
-                                System.out.println("\t Файл:" + file.getName());
-                            }
-                            FileUtils.calculateFolderSize(file.getPath());
-                        }
-                    } else {
-                        System.out.println("\n \t Файл:" + folder.getName());
-                        FileUtils.calculateFolderSize(folder.getPath());
-                    }
-                }
-            }
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
+        FileUtils.calculateFolderSize(path);
+        long total = FileUtils.calculateFolderSize(path);
+        System.out.println("Общий размер - " + FileUtils.friendlyFileSize(total));
     }
 }
+
