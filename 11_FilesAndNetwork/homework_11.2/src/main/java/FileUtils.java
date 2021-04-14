b8b4000041b5c2fefad7b111676b846771197467
HomeAdmin <xax_xax@list.ru>
2021-04-14 20:56:29 +0300

    WorkFilesAndNetwork2.0

--- 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,44 @@
+import java.io.*;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+    public static void copyFolder(String sourceDirectory, String destinationDirectory){
+        File source = new File(sourceDirectory);
+        File destination = new File(destinationDirectory);
+        if (source.isDirectory()) {
+            if (!destination.exists()) {
+                destination.mkdirs();
+            }
+
+            String files[] = source.list();
+            for (String file : files) {
+                File srcFile = new File(source, file);
+                File destFile = new File(destination, file);
+                copyFolder(srcFile.getPath(), destFile.getPath());
+            }
+        } else {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = new FileInputStream(source);
+                out = new FileOutputStream(destination);
+
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = in.read(buffer)) > 0) {
+                    out.write(buffer, 0, length);
+                }
+            } catch (Exception e) {
+                try {
+                    in.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                try {
+                    out.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
     }
 }
--- 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите путь к папке которую требуется скопировать: ");
+        Scanner scanner = new Scanner(System.in);
+        String source = scanner.nextLine();
+        System.out.println("Введите путь к папке в которую будет скопировано: ");
+        String destination = scanner.nextLine();
+        try {
+            FileUtils.copyFolder(source, destination);
+        }catch (Exception ex){
+         ex.printStackTrace();
+        }
     }
 }
