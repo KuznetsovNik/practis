0a449c6f24e765625b6c7c37d38c89b31bd04a85
HomeAdmin <xax_xax@list.ru>
2021-07-08 21:55:31 +0300

    ReworkFilesAndNetwork4.1.1

--- 11_FilesAndNetwork/homework_11.4/src/Main.java
+++ 11_FilesAndNetwork/homework_11.4/src/Main.java
@@ -2,8 +2,11 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.*;
+
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     private static final String WEB_SITE_URL = "https://lenta.ru";
@@ -13,46 +16,18 @@ public class Main {
         try {
             Document doc = Jsoup.connect(WEB_SITE_URL).get();
             Elements img = doc.getElementsByTag("img");
-
             for (Element el : img) {
                 String src = el.absUrl("src");
-                System.out.println("Image Found! : " + trim(el.attr("alt"), 20));
-                getImages(src);
+                System.out.println("Image Found! : " + el.attr("alt"));
+                try(InputStream in = new URL(src).openStream()){
+                    Files.copy(in, Paths.get(FOLDER_PATH));
+                }
             }
-        }catch(Exception ex) {
+        }catch(Exception ex){
             ex.printStackTrace();
         }
     }
-    private static String trim(String s, int width) {
-        if (s.length() > width)
-            return s.substring(0, width-1) + ".";
-        else
-            return s;
-    }
-
-    private static void getImages(String src) throws IOException {
-        int indexName = src.lastIndexOf("/");
-        if (indexName == src.length()) {
-            src = src.substring(1, indexName);
-        }
-        indexName = src.lastIndexOf("/");
-        String name = src.substring(indexName);
-        System.out.println(name);
-
-        URL url = new URL(src);
-        InputStream in = url.openStream();
-        // Паттерн Декоратор
-        OutputStream out = new BufferedOutputStream(new FileOutputStream(FOLDER_PATH + name));
-
-        for (int b; (b = in.read()) != -1; ) {
-            out.write(b);
-        }
-
-        out.close();
-        in.close();
-    }
 }
-
 
 
 
