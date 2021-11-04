14a8e6a373b83cec7ccac67363be996a01c3fa65
HomeAdmin <xax_xax@list.ru>
2021-11-04 22:17:53 +0300

    ReworkFilesAndNetwork4.1.2

--- 11_FilesAndNetwork/homework_11.4/src/Main.java
+++ 11_FilesAndNetwork/homework_11.4/src/Main.java
@@ -3,10 +3,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Main {
     private static final String WEB_SITE_URL = "https://lenta.ru";
@@ -19,14 +21,17 @@ public class Main {
             for (Element el : img) {
                 String src = el.absUrl("src");
                 System.out.println("Image Found! : " + el.attr("alt"));
-                try(InputStream in = new URL(src).openStream()){
-                    Files.copy(in, Paths.get(FOLDER_PATH));
+                try (InputStream in = new URL(src).openStream()) {
+                    Files.copy(in, Paths.get(FOLDER_PATH + "/" +
+                            new File(src.replaceAll(".*?([^/]*?\\.jpg|\\.jpeg|\\.gif|\\.png|\\.bmp|\\.js).*", "$1"))), StandardCopyOption.REPLACE_EXISTING);
                 }
             }
         }catch(Exception ex){
             ex.printStackTrace();
         }
     }
+
+
 }
 
 
